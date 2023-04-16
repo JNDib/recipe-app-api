47a01914bba717b273d25196ebda32d482b81bf8
Josh Diblin <20631465+JNDib@users.noreply.github.com>
2023-04-16 13:09:46 -0400

    update with wait_for_db checks

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,7 +8,6 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-    
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
@@ -17,6 +16,6 @@ class CalcTests(SimpleTestCase):
 
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
-        res = calc.Subtract(10, 15)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
